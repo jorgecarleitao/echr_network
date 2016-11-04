44ee9fb76ddfa9205196ccaef93beb403b631af2
Jorge C. Leitão <jorgecarleitao@gmail.com>
2016-11-04 10:38:16 +0100

    Added support for database migrations with Alembic.

--- run_crawler.py
+++ run_crawler.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-import crawler, models
+import crawler
 
 
 engine = create_engine('postgresql://echr:echr@localhost/echr', echo=False)
 Session = sessionmaker(bind=engine)
 
-# uncomment to drop all stuff and try again
-#models.Base.metadata.drop_all(engine)
-
-models.Base.metadata.create_all(engine)
 
 session = Session()
 
